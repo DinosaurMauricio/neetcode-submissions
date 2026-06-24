c6958ea32b5a3b5f89a9bb870cc8c361adc9ae14
DinosaurMauricio <70724167+DinosaurMauricio@users.noreply.github.com>
2026-06-24 11:07:50 -0600

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+
+        if len(nums) == 1:
+            return nums[0]
+
+
+        nums.sort()
+        major = nums[0]
+        counter = 1
+        p_counter = 0
+        res = -1
+
+        for i in range(1,len(nums)):
+
+            if nums[i] == major:
+                counter+=1
+            else:
+                major = nums[i]
+                counter = 1
+
+            if counter > p_counter:
+                p_counter = counter
+                major, res = nums[i],nums[i]
+                    
+                
+        return res
+
+
+        # solution is o(n) space
+        dict_counter = {}
+        num_major = 0
+        res = -1
+
+        for num in nums:
+            dict_counter[num] = dict_counter.get(num,0) + 1
+
+
+        #print(dict_counter)
+        for key, value in dict_counter.items():
+            if value > num_major:
+                num_major = value
+                res = key
+        
+        return res
+
+        
