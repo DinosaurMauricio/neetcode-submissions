adc068cd722b41c99f2a14dc6dfe4171f3fea5af
DinosaurMauricio <70724167+DinosaurMauricio@users.noreply.github.com>
2026-09-11 21:17:51 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,16 @@
+
+variable = 2
+print(type(variable))
+
+variable =2.2
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+
+variable = "h"
+print(type(variable))
+
+variable = []
+print(type(variable))
