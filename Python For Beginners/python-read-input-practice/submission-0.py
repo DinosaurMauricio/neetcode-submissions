6390a61c1f3658bcb2d37de93d008d1f08091957
DinosaurMauricio <70724167+DinosaurMauricio@users.noreply.github.com>
2026-09-25 21:02:50 -0600

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    inp = input()
+    l = list(map(int,inp.split(',')))
+    return sum(l)
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
